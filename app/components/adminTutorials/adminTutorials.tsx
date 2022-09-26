1943f529e2df395dca129645019835c8d3886e6c
Jose Fernan <jose.fernan@epitech.eu>
2022-09-26 15:04:38 +0900

    push to date

--- app/components/adminTutorials/adminTutorials.tsx
+++ app/components/adminTutorials/adminTutorials.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import TutorialWidget from './uploadWidgetTutorials'
 
 export default function adminTutorials() {
   return (
     <div className="usersAdmin-component">
         <div className="usersAdmin-title-div">
           <h1 className="usersAdmin-title">Tutorials</h1>
+        </div>
+        <div>
+          <TutorialWidget/>
         </div>
     </div>
   )
