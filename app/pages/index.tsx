b87e662a6d9285d574a95a469905c5422baa9861
Matisse Page <matisse.page@epitech.eu>
2022-12-16 07:02:39 -0800

    add dictionnary and translate

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/navbar";
 import Home from "../pages/home";
+import { LanguageProvider } from "../container/language"
 
 export default function App() {
   return (
     <>
-      <Home />
+      <LanguageProvider>
+        <Home />
+      </LanguageProvider>
     </>
   );
 }
--- app/pages/terms-of-use.tsx
+++ app/pages/terms-of-use.tsx
@@ -1,89 +1,54 @@
+import { useContext } from "react";
 import Footer from "../components/footer/footer";
 import Navbar from "../components/navbar/navbar";
+import { LanguageContext } from "../container/language";
 
 export default function TermsOfUse() {
+  const { dictionary } = useContext(LanguageContext);
+  
   return (
     <>
       <Navbar />
       <div id="terms-of-use-container">
-        <h2>Website Terms of Use</h2>
+        <h2>{dictionary["terms-of-use"].website.title}</h2>
 
         <div className="terms-of-use-box">
           <p>
-            The Block2School website located at www.block2school.fr is a
-            copyrighted work belonging to Block2School. Certain features of the
-            Site may be subject to additional guidelines, terms, or rules, which
-            will be posted on the Site in connection with such features.
+            {dictionary["terms-of-use"].website.first_content}
           </p>
 
           <p>
-            All such additional terms, guidelines, and rules are incorporated by
-            reference into these Terms.
+            {dictionary["terms-of-use"].website.second_content}
           </p>
 
           <p>
-            These Terms of Use described the legally binding terms and
-            conditions that oversee your use of the Site. BY LOGGING INTO THE
-            SITE, YOU ARE BEING COMPLIANT THAT THESE TERMS and you represent
-            that you have the authority and capacity to enter into these Terms.
-            YOU SHOULD BE AT LEAST 18 YEARS OF AGE TO ACCESS THE SITE. IF YOU
-            DISAGREE WITH ALL OF THE PROVISION OF THESE TERMS, DO NOT LOG INTO
-            AND/OR USE THE SITE.
+            {dictionary["terms-of-use"].website.third_content}
           </p>
 
           <p>
-            These terms require the use of arbitration Section 10.2 on an
-            individual basis to resolve disputes and also limit the remedies
-            available to you in the event of a dispute.
+            {dictionary["terms-of-use"].website.fourth_content}
           </p>
         </div>
-        <h2>Access to the Site</h2>
+        <h2>{dictionary["terms-of-use"].access_to_the_site.title}</h2>
         <div className="terms-of-use-box">
           <p>
-            <strong>Subject to these Terms.</strong> Company grants you a
-            non-transferable, non-exclusive, revocable, limited license to
-            access the Site solely for your own personal, noncommercial use.
+            <strong>{dictionary["terms-of-use"].access_to_the_site.first_strong_content}</strong> {dictionary["terms-of-use"].access_to_the_site.first_content}
           </p>
 
           <p>
-            <strong>Certain Restrictions.</strong> The rights approved to you in
-            these Terms are subject to the following restrictions: (a) you shall
-            not sell, rent, lease, transfer, assign, distribute, host, or
-            otherwise commercially exploit the Site; (b) you shall not change,
-            make derivative works of, disassemble, reverse compile or reverse
-            engineer any part of the Site; (c) you shall not access the Site in
-            order to build a similar or competitive website; and (d) except as
-            expressly stated herein, no part of the Site may be copied,
-            reproduced, distributed, republished, downloaded, displayed, posted
-            or transmitted in any form or by any means unless otherwise
-            indicated, any future release, update, or other addition to
-            functionality of the Site shall be subject to these Terms. All
-            copyright and other proprietary notices on the Site must be retained
-            on all copies thereof.
+            <strong>{dictionary["terms-of-use"].access_to_the_site.second_strong_content}</strong> {dictionary["terms-of-use"].access_to_the_site.second_content}
           </p>
 
           <p>
-            Company reserves the right to change, suspend, or cease the Site
-            with or without notice to you. You approved that Company will not be
-            held liable to you or any third-party for any change, interruption,
-            or termination of the Site or any part.
+            {dictionary["terms-of-use"].access_to_the_site.third_content}
           </p>
 
           <p>
-            <strong>No Support or Maintenance.</strong> You agree that Company
-            will have no obligation to provide you with any support in
-            connection with the Site.
+            <strong>{dictionary["terms-of-use"].access_to_the_site.fourth_strong_content}</strong> {dictionary["terms-of-use"].access_to_the_site.fourth_content}
           </p>
 
           <p>
-            Excluding any User Content that you may provide, you are aware that
-            all the intellectual property rights, including copyrights, patents,
-            trademarks, and trade secrets, in the Site and its content are owned
-            by Company or Company's suppliers. Note that these Terms and access
-            to the Site do not give you any rights, title or interest in or to
-            any intellectual property rights, except for the limited access
-            rights expressed in Section 2.1. Company and its suppliers reserve
-            all rights not granted in these Terms.
+            {dictionary["terms-of-use"].access_to_the_site.fifth_content}
           </p>
         </div>
         <h2>User Content</h2>
