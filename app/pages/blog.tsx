6d1ed9d5d7cf50476f3111e9c4a147667f3c2ed5
lucase84 <lucas.dudot@epitech.eu>
2023-01-20 16:26:11 +0100

    Adding a modal to ask a question to other users

--- app/pages/blog.tsx
+++ app/pages/blog.tsx
@@ -4,6 +4,9 @@ import Navbar from '../components/navbar/navbar'
 import moment from 'moment'
 import { serverURL } from '../utils/globals'
 import ListBlog from '../components/blog/blogList'
+import Footer from '../components/footer/footer'
+import $ from 'jquery'
+import { Spacer } from '@chakra-ui/react'
 
 interface BlogProps {
   id: number
@@ -31,36 +34,103 @@ export default function Blog() {
 
   useEffect(() => {
     setIsLoading(true)
-    axios.get(`${serverURL}:8080/article/all`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      },
-    }).then((res) => {
-      console.log('articles: ', res.data.data)
-      setArticles(
-        res.data.data
-          ?.sort((a: BlogProps, b: BlogProps) => b.id - a.id)
-          .map((item: BlogProps) => {
-            return {
-              ...item,
-              publicationDate: moment(item.publicationDate * 1000).format(
-                'LLL',
-              ),
-              editDate: moment(item.editDate * 1000).format('LLL'),
-            }
-          }),
-      )
-      setIsLoading(false)
-    })
+    axios
+      .get(`${serverURL}:8080/article/all`, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
+      .then((res) => {
+        console.log('articles: ', res.data.data)
+        setArticles(
+          res.data.data
+            ?.sort((a: BlogProps, b: BlogProps) => b.id - a.id)
+            .map((item: BlogProps) => {
+              return {
+                ...item,
+                publicationDate: moment(item.publicationDate * 1000).format(
+                  'LLL',
+                ),
+                editDate: moment(item.editDate * 1000).format('LLL'),
+              }
+            }),
+        )
+        setIsLoading(false)
+      })
   }, [])
+
+  function openModal() {
+    $('#blog-contact-title-input').val('')
+    $('#blog-contact-description-input').val('')
+    $('#blog-contact-modal').css('display', 'block')
+  }
+
+  function closeModal() {
+    $('#blog-contact-modal').css('display', 'none')
+  }
+
+  function submit() {
+    // axios
+    //   .post(
+    //     `${serverURL}:8080/article/report`,
+    //     {
+    //       title: $('#blog-contact-title-input').val(),
+    //       description: $('#blog-contact-description-input').val(),
+    //     },
+    //     {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         'Access-Control-Allow-Origin': '*',
+    //       },
+    //     },
+    //   )
+    //   .then((res) => {
+    //     if (res.status === 200) {
+    closeModal()
+    //   }
+    // })
+  }
 
   return (
     <>
       <Navbar />
-      <div style={{ height: '100%', minHeight: '90vh', backgroundColor: '#343434' }}>
-        <ListBlog articles={articles}/>
+      <div id="blog-container">
+        <ListBlog articles={articles} />
       </div>
-    </>  
-  );
+      <button id="blog-contact-button" onClick={openModal}>
+        Ask a question
+      </button>
+      <div id="blog-contact-modal">
+        <div id="blog-contact-modal-header">
+          <h1>Ask a question</h1>
+          <Spacer />
+          <span
+            id="blog-contact-modal-close"
+            style={{
+              cursor: 'pointer',
+            }}
+            onClick={closeModal}
+          >
+            &times;
+          </span>
+        </div>
+        <div id="blog-contact-body">
+          <span id="blog-contact-title">Subject</span>
+          <input type="text" id="blog-contact-title-input" />
+          <span id="blog-contact-description">Description</span>
+          <textarea
+            id="blog-contact-description-input"
+            style={{
+              resize: 'none',
+            }}
+          />
+          <button id="blog-contact-button-submit" onClick={submit}>
+            Submit
+          </button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
