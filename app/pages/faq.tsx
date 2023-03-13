8a9ae927f74e20af0a2145b4279a887156019a74
Lucase84 <lucas.dudot@epitech.eu>
2023-03-13 16:57:47 +0100

    Add some question and implement a dynamic search bar to explore FAQ

--- app/pages/faq.tsx
+++ app/pages/faq.tsx
@@ -1,8 +1,51 @@
-import Footer from '../components/footer/footer'
-import Navbar from '../components/navbar/navbar'
-import Question from '../components/faq/faqQuestion'
+import Footer from "../components/footer/footer";
+import Navbar from "../components/navbar/navbar";
+import Question from "../components/faq/faqQuestion";
+import { useState } from "react";
 
 export default function FAQ() {
+  const [searchInput, setSearchInput] = useState("");
+  const questionList = [
+    {
+      question: "What is Blockchain ?",
+      answer:
+        "Blockchain is a distributed ledger that is a public ledger oftransactions. It is a system of record that is open, immutable, and verifiable.",
+    },
+    {
+      question: "What is a smart contract ?",
+      answer:
+        "A smart contract is a contract that is written in code and executed on a blockchain.",
+    },
+    {
+      question: "What is ERC20 norm ?",
+      answer: "ERC20 is a standard for tokenization of digital assets.",
+    },
+    {
+      question: "What is a token ?",
+      answer:
+        "A token is a digital asset that is issued by a blockchain company.",
+    },
+    {
+      question: "What is a wallet ?",
+      answer: "A wallet is a software that stores your private keys.",
+    },
+    {
+      question: "Is the website secure ?",
+      answer:
+        "Yes, the website is secure. We use the latest security protocols to ensure that your data is safe.",
+    },
+    {
+      question: "What is a transaction ?",
+      answer:
+        "A transaction is a movement of funds from one address to another.",
+    },
+  ];
+
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
   return (
     <>
       <Navbar />
@@ -11,34 +54,49 @@ export default function FAQ() {
           <span>Frequently Asked Questions</span>
         </div>
         <div id="faq-body">
+          <div
+            style={{
+              width: "20%",
+              height: "40px",
+            }}
+          >
+            <input
+              id="faq-search"
+              placeholder="Search for a question"
+              onChange={handleChange}
+              value={searchInput}
+            />
+          </div>
           <div id="faq-questions">
-            <Question
-              question="What is Blockchain ?"
-              answer="Blockchain is a distributed ledger that is a public
-                      ledger oftransactions. It is a system of record that
-                      is open, immutable, and verifiable."
-            />
-            <Question
-              question="What is a smart contract ?"
-              answer="A smart contract is a contract that is written in code and
-              executed on a blockchain."
-            />
-            <Question
-              question="What is ERC20 norm ?"
-              answer="ERC20 is a standard for tokenization of digital assets."
-            />
-            <Question
-              question="What is a token ?"
-              answer="A token is a digital asset that is issued by a blockchain
-              company."
-            />
-            <Question question="TEST" answer="test"/>
-            <Question question="TEST" answer="test"/>
-            <Question question="TEST" answer="test"/>
+            {questionList.map((question, index) => {
+              if (searchInput.length > 0) {
+                if (
+                  question.question
+                    .toLowerCase()
+                    .includes(searchInput.toLowerCase())
+                ) {
+                  return (
+                    <Question
+                      key={index}
+                      question={question.question}
+                      answer={question.answer}
+                    />
+                  );
+                }
+              } else {
+                return (
+                  <Question
+                    key={index}
+                    question={question.question}
+                    answer={question.answer}
+                  />
+                );
+              }
+            })}
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
