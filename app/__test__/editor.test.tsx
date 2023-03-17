509e5eae119b5ca3b446cab653d5fbd535fa514f
Matisse Page <matisse.page@epitech.eu>
2023-03-16 19:25:02 -0700

    add unit_test for editor

--- app/__test__/editor.test.tsx
+++ app/__test__/editor.test.tsx
@@ -6,24 +6,37 @@ import Adapter from "enzyme-adapter-react-16";
 configure({ adapter: new Adapter() });
 import { shallow } from 'enzyme';
 import { render, screen } from '@testing-library/react';
+import OptionEditor from '../components/editor/optionEditor';
 
 
-describe('<MyEditor/>', () => {
+describe('<OptionEditor/>', () => {
   let wrapper: any;
 
   test('should render correctly with no arg', () => {
-    wrapper = shallow(<MyEditor theme={undefined} lang={undefined} onChange={undefined} defaultValue={undefined} />)
+    wrapper = shallow(<OptionEditor changeLang={undefined} scoring={undefined} switchText={undefined} changeTheme={undefined} />)
   });
 
-  test('should render correctly with args', () => {
-    const theme = 'vs-dark';
-    const lang = 'javascript';
-    shallow(<MyEditor 
-      theme={theme} 
-      lang={lang} 
-      onChange={undefined}
-      defaultValue={''} />)
+  test('should render a optionEditor component', () => {
+    expect(wrapper.find('#editor_opt').length).toEqual(1)
   })
+
+  test('should render a OptionEditor component', () => {
+    expect(wrapper.find('#upload').length).toEqual(1)
+  })
+
+  test('should render a OptionEditor component', () => {
+    expect(wrapper.find('#lang_choice').length).toEqual(1)
+  })
+
+  // test('should render correctly with args', () => {
+  //   const theme = 'vs-dark';
+  //   const lang = 'javascript';
+  //   shallow(<MyEditor 
+  //     theme={theme} 
+  //     lang={lang} 
+  //     onChange={undefined}
+  //     defaultValue={''} />)
+  // })
 
   test('match snapshot', () => {
     expect(wrapper).toMatchSnapshot();
