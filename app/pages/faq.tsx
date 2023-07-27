becfe9b5658d827c5a4a2db204311624df9271bb
Lucase84 <lucas.dudot@epitech.eu>
2023-07-27 16:49:59 +0200

    Add autocomplete to faq search bar

--- app/pages/faq.tsx
+++ app/pages/faq.tsx
@@ -24,18 +24,100 @@ export default function FAQ() {
       },
     }).then((res) => {
       setFaqList(res.data)
+      autocomplete(document.getElementById("faq-search") as HTMLInputElement, res.data.map((faq: any) => faq.question));
     })
   }
-
-  const handleChange = (e: any) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-  };
 
   const bg = useColorModeValue("#191919", "white");
   const color = useColorModeValue("black", "black");
 
   const { dictionary } = useContext(LanguageContext);
+
+  function autocomplete(inp: HTMLInputElement, arr: string[]) {
+    let currentFocus: number;
+  
+    inp.addEventListener("input", function (e: Event) {
+      const val = (this).value;
+      closeAllLists();
+      if (!val) {
+        setSearchInput("");
+        return false;
+      }
+      currentFocus = -1;
+  
+      const a = document.createElement("DIV");
+      a.setAttribute("id", (this).id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      (this).parentNode?.appendChild(a);
+  
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i].toLocaleLowerCase().includes(val.toLocaleLowerCase())) {
+          const b = document.createElement("DIV");
+          let index = arr[i].toLowerCase().indexOf(val.toLowerCase());
+          b.innerHTML = arr[i].substr(0, index);
+          b.innerHTML += "<strong>" + arr[i].substr(index, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(index + val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+  
+          b.addEventListener("click", function (e: Event) {
+            const input = this.getElementsByTagName("input")[0];
+            inp.value = input.value;
+            setSearchInput(input.value);
+            closeAllLists();
+          });
+  
+          if (a.childElementCount <= 9)
+            a.appendChild(b);
+        }
+      }
+    });
+  
+    inp.addEventListener("keydown", function (e: KeyboardEvent) {
+      const x = document.getElementById((this).id + "autocomplete-list");
+      if (x) {
+        const divElements = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+          currentFocus++;
+          addActive(divElements);
+        } else if (e.keyCode == 38) {
+          currentFocus--;
+          addActive(divElements);
+        } else if (e.keyCode == 13) {
+          e.preventDefault();
+          if (currentFocus > -1) {
+            if (x) divElements[currentFocus].click();
+          }
+        }
+      }
+    });
+  
+    function addActive(x: HTMLCollectionOf<Element>) {
+      if (!x) return false;
+      removeActive(x);
+      if (currentFocus >= x.length) currentFocus = 0;
+      if (currentFocus < 0) currentFocus = x.length - 1;
+      x[currentFocus].classList.add("autocomplete-active");
+    }
+  
+    function removeActive(x: HTMLCollectionOf<Element>) {
+      for (let i = 0; i < x.length; i++) {
+        x[i].classList.remove("autocomplete-active");
+      }
+    }
+  
+    function closeAllLists(elmnt?: Element) {
+      const x = document.getElementsByClassName("autocomplete-items");
+      for (let i = 0; i < x.length; i++) {
+        if (elmnt != x[i] && elmnt != inp) {
+          x[i].parentNode?.removeChild(x[i]);
+        }
+      }
+    }
+  
+    document.addEventListener("click", function (e: Event) {
+      closeAllLists(e.target as Element);
+    });
+  }  
 
   return (
     <>
@@ -57,8 +139,6 @@ export default function FAQ() {
             <input
               id="faq-search"
               placeholder="Search for a question"
-              onChange={handleChange}
-              value={searchInput}
             />
           </div>
           <div id="faq-questions">
