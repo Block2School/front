cb201162e391be91fd555d7799d263e9855c4dda
gabriel knies <gabriel.knies@epitech.eu>
2023-12-07 02:48:13 +0100

    challenges: adding modifications

--- app/pages/challengesTemp/[roomID].tsx
+++ app/pages/challengesTemp/[roomID].tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import axios from "axios";
 import React from 'react';
 import Navbar from "../../components/navbar/navbar";
@@ -9,7 +10,7 @@ export default function setUUID() {
 
   const [currentUrl, setCurrentUrl] = useState("")
   const router = useRouter()
-  const [player, setPlayer] = useState<{username: string, uuid:string, points: string}>({username:"", uuid:"lorenzo", points:""})
+  const [player, setPlayer] = useState<{username: string, uuid:string, points: string}>({username:"", uuid:"", points:""})
 
   useEffect(() => {
     console.log(sessionStorage.getItem('token'))
@@ -17,27 +18,34 @@ export default function setUUID() {
       setCurrentUrl(window.location.href);
       console.log('URL: ', currentUrl);
     }
-    buildPlaceChallengeRoom()
+    const onRoomJoining = async () => {
+      await fetchProfile();
+    }
+    onRoomJoining();
   }, [currentUrl]);
 
-  function fetchProfile() {
-    axios.get(`${serverURL}:8080/user/profile`, {
+
+  useEffect(() => {
+    console.log("PLAYER", player);
+    buildPlaceChallengeRoom()
+  }, [player.uuid])
+
+  const fetchProfile = async () => {
+    let res = await axios.get(`${serverURL}:8080/user/profile`, {
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
           Authorization: 'Bearer ' + sessionStorage.getItem('token'),
         },
       })
-      .then((res) => {
-        if (res.status === 200) {
-          setPlayer({username: res.data.username, uuid: res.data.uuid, points: res.data.points})
-        }
-      })
+      if (res.status === 200) {
+        setPlayer({username: res.data.username, uuid: res.data.uuid, points: res.data.points})
+      }
   }
-  // href="/challengesTemp/[roomID]/[uuid]" as={`/challengesTemp/${roomID}/${uuid}`}
+
   function buildPlaceChallengeRoom() {
     if (player.uuid != "" && currentUrl !="") {
-      console.log(currentUrl + "/" + player.uuid)
+      console.log('BBUILDPLACECHALLENGE', currentUrl + "/" + player.uuid)
       router.replace(currentUrl + "/" + player.uuid)
     }
   }
--- app/pages/challengesTemp/[roomID]/[uuid].tsx
+++ app/pages/challengesTemp/[roomID]/[uuid].tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Navbar from "../../../components/navbar/navbar";
-import { Text, SimpleGrid, Box } from "@chakra-ui/react";
+import { Text, SimpleGrid, Box, useClipboard } from "@chakra-ui/react";
 import { serverURL, webSocketURL } from "../../../utils/globals";
 import { Button } from "react-bootstrap";
 import { useRouter } from "next/router";
@@ -11,16 +11,31 @@ export default function ChallengesTemp() {
   const router = useRouter();
   const [room, setRoom] = useState<{
       master: string,
-      occupants?: [number],
+      occupants: [{
+        userId: string,
+        username: string,
+      }],
       maxTime: number,
       limitUser: number
-  }>({ master: '', occupants: undefined, maxTime: 10, limitUser: 4 });
+  }>({ master: '', occupants: [{userId:'', username:''}], maxTime: 10, limitUser: 4 });
   const { roomID, uuid } = router.query;
   const [time, setTime] = useState({minutes: 5, seconds: 0});
   const [ws, setWs] = useState<WebSocket | null>(null);
   let check : boolean = false;
   const [roomJoined, setRoomJoined] = useState(false);
   const [player, setPlayer] = useState<{username: string, uuid:string, points: string}>({username:"", uuid:"", points:""})
+  
+  
+  function getRoomLink () {
+    let rawLink = (window != undefined) ? window?.location?.href : '';
+    console.log('rawLink: ', rawLink);
+    const lastPos = rawLink?.lastIndexOf('/');
+    let link = rawLink?.substring(0, lastPos);
+    
+    console.log('link: ', link);
+    return link;
+  }
+  const { onCopy, value, hasCopied} = useClipboard(`http://localhost:3000/challengesTemp/` + roomID);
 
   useEffect(() => {
 
@@ -36,7 +51,7 @@ export default function ChallengesTemp() {
     const timer = setInterval(() => {
       setTime(prevTime => {
         if (prevTime.minutes === 0 && prevTime.seconds === 0) {
-          //router.push("/challenge")
+          router.push("/challenge")
           clearInterval(timer);
           return prevTime;
         }
@@ -52,9 +67,10 @@ export default function ChallengesTemp() {
   }, []);
 
   useEffect(() => {
-    if (room.master != '' && roomJoined === false) {
+    if (roomJoined === false) {
       joinRoom();
       setRoomJoined(true);
+      fetchRoom();
     }
   }, [room])
 
@@ -82,7 +98,7 @@ export default function ChallengesTemp() {
           'Authorization': `Bearer ${sessionStorage.getItem('token')}`,
         },
       })
-    console.log(res.data);
+    console.log('fetchROOM[RES.DATA]: ', res.data);
     setRoom(res.data);
   }
 
@@ -105,20 +121,52 @@ export default function ChallengesTemp() {
   function parseMessage(message: string) {
     const data = JSON.parse(message);
     switch (data.type) {
+      case "new user joined":
+        console.log('newUserJoined: Room: ', room);
+        console.log('newUserJoined: ', data.message);
+        let _myRoom: any = {};
+        Object.assign(_myRoom, room);
+        _myRoom.occupants.push(data.message);
+        setRoom(_myRoom);
+        break;
       case "room info":
+        console.log('roomInfo: ', data.message);
         console.log("temps => " + data.message.remainingTime)
+        if (data.message?.remainingTime == undefined) break;
         setTime({minutes: Math.floor(data.message.remainingTime / 60), seconds: data.message.remainingTime % 60 })
+
+        let myRoom: any = {};
+        console.log('room: ', room);
+        Object.assign(myRoom, room);
+        Object.assign(myRoom.occupants, data.message.occupants);
+        setRoom(myRoom);
+        break;
       case "joinRoom":
-        console.log("joined room");
+        console.log("joined room: ", data.message);
+        // update room (only update occupants)
+        let myRoomData: any = {};
+        Object.assign(myRoomData, room);
+        Object.assign(myRoomData.occupants, data.message.occupants);
+        setRoom(myRoomData);
+        break;
+      case "userLeft":
+        console.log('userLeft: ', data.message);
+        let _myRoom2: any = {};
+        Object.assign(_myRoom2, room);
+        _myRoom2.occupants = data.message.occupants;
+        setRoom(_myRoom2);
         break;
       case "leaveRoom":
-        console.log("left room");
+        console.log("left room", data.message);
+        // NOTHING TO DO.
         break;
       case "startGame":
         console.log("starting game");
+        // TODO: start game
         break;
       case "endGame":
         console.log("ending game");
+        // TODO: end game
         break;
       default:
         console.log("unknown message");
@@ -205,20 +253,19 @@ export default function ChallengesTemp() {
         <Navbar/>
         <div id="content" style={{flexDirection:"column",justifyContent:"center", alignItems:"center",
              height:"calc(100vh - 95px)", width:"100%"}}>
-          <Button onClick={() => inviteFriends()}>Invite Friends</Button>
+          <Button onClick={onCopy}>Copier le lien</Button>
           <div id="time" style={{color:"#ffe6c4" , fontSize:"85px", justifyContent:"center", alignItems:"center", height:"175px"}}>
             {String(time.minutes).padStart(2, '0')}:{String(time.seconds).padStart(2, '0')}
           </div>
           <div id="users" style={{height:"50%", width:"50%"}}>
             <SimpleGrid columns={2} gap={6} height={"100%"}>
-              <Box bg='tomato'> User Here</Box>
-              <Box bg='tomato'> User Here</Box>  {/* Réfléchir implémentation Ami dans Box */}
-              <Box bg='tomato'> User Here</Box>
-              <Box bg='tomato'> User Here</Box>
+              <Box bg='tomato'>{room.occupants.at(0)?.username}</Box>
+              <Box bg='tomato'>{room.occupants.at(1)?.username}</Box>  {/* Réfléchir implémentation Ami dans Box */}
+              <Box bg='tomato'>{room.occupants.at(2)?.username}</Box>
+              <Box bg='tomato'>{room.occupants.at(3)?.username}</Box>
             </SimpleGrid>
           </div>
           <div id="options" style={{display:"flex", justifyContent:"space-evenly", alignItems:"center",width:"50%" ,height:"150px"}}>
-            <Button onClick={() => fetchRoom()}>Ready</Button>
             <Button onClick={() => quitLobby()}>Quitter</Button>
           </div>
         </div>
