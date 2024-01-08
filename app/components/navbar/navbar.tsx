4e90bfbea60d925ce65aa7022ae07aae754c88c2
Jose Fernan <jose.fernan@epitech.eu>
2024-01-08 13:04:07 +0100

    Adding navbar v2

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -16,6 +16,10 @@ export default function Navbar() {
 
   const { dictionary } = useContext(LanguageContext);
   const [showModal, setShowModal] = useState(false);
+  const [showCodeHereModal, setCodeHereModal] = useState(false)
+  const [showCommunityModal, setCommunityModal] = useState(false)
+  const [showInformationModal, setInformationModal] = useState(false)
+  const [active, setActiveyar] = useState(true)
 
 
   const {
@@ -24,11 +28,35 @@ export default function Navbar() {
     account,
     activate,
     deactivate,
-    active,
+    // active,
   } = useWeb3React()
 
   const refreshState = () => {
     window.sessionStorage.setItem('provider', 'undefined')
+  }
+
+  const toggleCodeHereModal = () => {
+    setCodeHereModal(true);
+  }
+
+  const toggleCodeHereModalLeave = () => {
+    setCodeHereModal(false);
+  }
+
+  const toggleCommunityModal = () => {
+    setCommunityModal(true);
+  }
+
+  const toggleCommunityModalLeave = () => {
+    setCommunityModal(false);
+  }
+
+  const toggleInformationModal = () => {
+    setInformationModal(true);
+  }
+
+  const toggleInformationModalLeave = () => {
+    setInformationModal(false);
   }
 
   const disconnect = () => {
@@ -55,54 +83,90 @@ export default function Navbar() {
     if (provider) activate(connectors[provider], handleConnection)
   }, [])
 
+
   return (
     <>
       <div id="navbar-component">
         <div id="navbar-container">
           <div id="navbar-logo-container">
             <Image id='navbar-logo' src="/Logo_B2S.png" alt="logo" height="50" width="50" />
-            <Link  href={'/'} passHref>
+            <Link  href={'/home'} passHref>
               <span className="navbar-text-logo">Block2School</span>
             </Link>
           </div>
           <div id="navbar-links-container">
             <div id="navbar-links-main">
-              <Link href={'/tutorials'} passHref>
-                <span className="navbar-text">{dictionary.navbar.tutorials}</span>
-              </Link>
-              <Link href={'/challenges'} passHref>
-                <span className="navbar-text">Challenges</span>
-              </Link>
-              <Link href={'/faq'} passHref>
-                <span className="navbar-text">FAQ</span>
-              </Link>
-              {/* <Link href={'/login'} passHref>
-                <span className="navbar-text">Login</span>
-              </Link> */}
-              <Link href={'/blog'} passHref>
-                <span className="navbar-text">Blog</span>
-              </Link>
-              <Link href={'/marketplace'} passHref>
-                <span className="navbar-text">Marketplace</span>
-              </Link>
+              <div className="scolldown-item">
+                <span id="navbar-drop-title" className='navbar-text' onMouseEnter={toggleCodeHereModal} onMouseLeave={toggleCodeHereModalLeave} >Codehere</span>
+                { showCodeHereModal && (
+                <div onMouseEnter={toggleCodeHereModal} onMouseLeave={toggleCodeHereModalLeave} className='dropdownContent'>
+                  <Link href={'/tutorials'} passHref>
+                    <div id='navbar-drop-link'>
+                      <span className="navbar-text">Tutorials</span>
+                    </div>
+                  </Link>             
+                  <Link href={'/challenges'} passHref>
+                  <div id='navbar-drop-link'>
+                      <span className="navbar-text">Challenges</span>
+                    </div>                  
+                  </Link>
+                </div>
+                )}
+              </div>
+              <div className="scolldown-item">
+                <span id="navbar-drop-title" className='navbar-text' onMouseEnter={toggleCommunityModal} onMouseLeave={toggleCommunityModalLeave}>Community</span>
+                { showCommunityModal && (
+                <div onMouseEnter={toggleCommunityModal} onMouseLeave={toggleCommunityModalLeave} className='dropdownContent'>
+                  <Link href={'/marketplace'} passHref>
+                  <div id='navbar-drop-link'>
+                      <span className="navbar-text">Marketplace</span>
+                    </div>                  
+                  </Link>             
+                  <Link href={'/forum'} passHref>
+                  <div id='navbar-drop-link'>
+                      <span className="navbar-text">Forum</span>
+                    </div>                  
+                  </Link>
+                </div>
+                )}
+              </div>
+              <div className="scolldown-item">
+                <span  id="navbar-drop-title" className='navbar-text' onMouseEnter={toggleInformationModal} onMouseLeave={toggleInformationModalLeave}>Information</span>
+                { showInformationModal && (
+                <div onMouseEnter={toggleInformationModal} onMouseLeave={toggleInformationModalLeave} className='dropdownContent'>
+                  <Link href={'/faq'} passHref>
+                  <div id='navbar-drop-link'>
+                      <span className="navbar-text">FAQ</span>
+                    </div>                  
+                  </Link>             
+                  <Link href={'/terms-of-use'} passHref>
+                  <div id='navbar-drop-link'>
+                      <span className="navbar-text">Terms of Use</span>
+                    </div>                  
+                  </Link>
+                  <Link href={'/privacy-policy'} passHref>
+                  <div id='navbar-drop-link'>
+                      <span className="navbar-text">Privacy Policy</span>
+                    </div>                      
+                    </Link>
+                </div>
+                )}
+              </div>
             </div>
           </div>
           <div id="navbar-mobile-part">
-            <NavbarAllBalances />
             {
               active === true ?
-                <Link href={'/profile'} passHref>
-                  <BiUser
-                    size={"13%"}
-                    color={"white"}
-                    style={{ padding: '15px', cursor: 'pointer',}}
-
-                  />
-                </Link>
+              <div>
+                <NavbarAllBalances/>
+              </div>
                 :
                 null
             }
-            <ConnectionButton />
+            {
+              active === true ? <ConnectionButton /> : null
+
+            }
             <ThemeSelector />
             <LanguageSwitcher />
           </div>
